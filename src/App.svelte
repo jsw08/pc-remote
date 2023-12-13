96205a92ea2a0f177a3a3958d1cf0c41e730c62f
jsw08 <jurnwubben@gmail.com>
2023-12-13 18:20:47 +0100

    bit more progress on the css

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,20 @@
 <script>
-  let powered = true;
-  let loading = false;
+  let mode = "on"
   const base_css = "bg-BASE-500 b-BASE hover:bg-BASE-400";
-  const green_css = base_css.replaceAll("BASE", "green")
-  const red_css = base_css.replaceAll("BASE", "red")
-  const load_css = base_css.replaceAll("BASE", "gray")
+
+  const colors = {
+    "on": "green",
+    "booting": "orange",
+    "off": "red",
+    "loading": "gray"
+  }
+  $: c = colors[mode]
 </script>
 
 <main class="w-full h-screen flex justify-center items-center">
   <button
-    class={"button w-xs h-xs sm:w-sm sm:h-sm md:w-lg md:h-lg text-white font-bold py-8 px-12 b-b-12 active:b-b-4 rounded cursor-pointer " +
-      (loading ? load_css : powered ? green_css : red_css)}
-    on:mouseup={() => powered = !powered}
+    class="button w-xs h-xs sm:w-sm sm:h-sm md:w-lg md:h-lg text-white font-bold py-8 px-12 b-b-12 active:b-b-4 rounded cursor-pointer bg-{c}-500 hover:bg-{c}-4"
+    on:click={() => mode = mode == "on" ? "off" : "on"}
   >
     <i class="i-material-symbols-power-plug w-full h-full text-5xl block"></i>
   </button>
