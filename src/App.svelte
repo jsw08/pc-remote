c9939ef38cccd61c3ff2416ef3c28d384d931b56
jsw08 <jurnwubben@gmail.com>
2023-12-24 16:48:39 +0100

    almost finished

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@
   const handleLogin = (): void => {
     $channel = value;
     value = "";
-    state = requestState();
+    cpromise = requestState();
   };
 
   const maxWait: number = 10 * 1000; // max timeout is 10s
@@ -15,34 +15,34 @@
         method: "POST",
         body: body,
       })
-        .then(async (statusReq) => {
-          if (!statusReq.ok) {
-            const err = await statusReq.text();
+        .then(async (res) => {
+          if (!res.ok) {
+            const err = await res.text();
             console.error(err);
             reject(
               new Error("Something went wrong while asing for the status.")
             );
           }
 
-          let statusESS = new EventSource(`https://ntfy.sh/${$channel}/sse`);
-          const statusESSTimeout = setTimeout(() => {
-            statusESS.close();
+          let sse = new EventSource(`https://ntfy.sh/${$channel}/sse`);
+          const sseTimeout = setTimeout(() => {
+            sse.close();
             reject(new Error(`Response took too long. Over ${maxWait}ms.`));
           }, maxWait);
-          statusESS.onerror = (e) => {
+          sse.onerror = (e) => {
             console.error(e);
             reject(
               new Error("Something went wrong while listening for a response.")
             );
           };
-          statusESS.onmessage = (e) => {
+          sse.onmessage = (e) => {
             const data = JSON.parse(e.data);
             console.log(data);
             if (!data.message) return;
             if (!data.message.includes(query)) return;
 
-            clearTimeout(statusESSTimeout);
-            statusESS.close();
+            clearTimeout(sseTimeout);
+            sse.close();
             resolve(data.message);
           };
         })
@@ -54,25 +54,26 @@
     });
   const requestState = (): Promise<boolean> =>
     request("status", "res s").then((v) => (v[6] === "1" ? true : false));
-  let state: Promise<boolean> | undefined = $channel
-    ? requestState()
-    : undefined;
 
   const togglePower = (cstate: boolean | undefined): Promise<boolean> =>
     request("press", "pressed").then((v) => !cstate);
-  const logout = () => $channel = "";
+  const logout = () => ($channel = "");
+  let cpromise: Promise<boolean> | undefined = $channel
+    ? requestState()
+    : undefined;
 </script>
 
-{#await state}
+{#await cpromise}
   <button class="btn btn-neutral btn" disabled
     ><span class="loading loading-ring loading-lg"></span></button
   >
 {:then cstate}
   <button
+    data-state={cstate}
     class="btn"
     class:btn-success={cstate}
     class:btn-error={!cstate}
-    on:click={() => (state = togglePower(cstate))}
+    on:click={() => (cpromise = togglePower(cstate))}
     ><svg
       xmlns="http://www.w3.org/2000/svg"
       width="32"
@@ -100,28 +101,41 @@
     >
     <span>ERROR: {error}</span>
     <div>
-      <button class="btn btn-sm" on:click={logout}>Logout</button
-      >
+      <button class="btn btn-sm" on:click={logout}>Logout</button>
       <button
         class="btn btn-sm btn-primary"
-        on:click={() => (state = requestState())}>Retry</button
+        on:click={() => (cpromise = requestState())}>Reload</button
       >
     </div>
   </div>
 {/await}
 
-<button class="btn btn-square btn-outline absolute right-0 top-0 m-3" on:click={logout}
-  ><svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    ><path
-      fill="currentColor"
-      d="M14.08 15.59L16.67 13H7v-2h9.67l-2.59-2.59L15.5 7l5 5l-5 5zM19 3a2 2 0 0 1 2 2v4.67l-2-2V5H5v14h14v-2.67l2-2V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.11.89-2 2-2z"
-    /></svg
-  ></button
->
+<div class="absolute right-0 bottom-0 sm:top-3 p-3">
+  <button class="btn btn-square btn-outline" on:click={() => cpromise = requestState()}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      ><path
+        fill="currentColor"
+        d="M12 20q-3.35 0-5.675-2.325T4 12q0-3.35 2.325-5.675T12 4q1.725 0 3.3.712T18 6.75V4h2v7h-7V9h4.2q-.8-1.4-2.187-2.2T12 6Q9.5 6 7.75 7.75T6 12q0 2.5 1.75 4.25T12 18q1.925 0 3.475-1.1T17.65 14h2.1q-.7 2.65-2.85 4.325T12 20"
+      /></svg
+    >
+  </button>
+  <button class="btn btn-square btn-outline" on:click={logout}
+    ><svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      ><path
+        fill="currentColor"
+        d="M14.08 15.59L16.67 13H7v-2h9.67l-2.59-2.59L15.5 7l5 5l-5 5zM19 3a2 2 0 0 1 2 2v4.67l-2-2V5H5v14h14v-2.67l2-2V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.11.89-2 2-2z"
+      /></svg
+    ></button
+  >
+</div>
 
 <dialog
   id="my_modal_5"
@@ -146,7 +160,7 @@
           type="text"
           bind:value
         />
-        <button class="btn btn-primary" type="submit">Submit</button>
+        <button class="btn btn-primary" type="submit">Login</button>
       </form>
     </div>
   </div>
